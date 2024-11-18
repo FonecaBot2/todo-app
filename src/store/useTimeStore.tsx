29d20e4e82e7add25c5d9ee53dae931ae0d8d22b
FonecaBot2 <robertorve27@gmail.com>
2024-11-18 10:00:01 -0300

    update store to use local date

--- src/store/useTimeStore.tsx
+++ src/store/useTimeStore.tsx
@@ -6,16 +6,9 @@ type TimeStore = {
 };
 
 export const useTimeStore = create<TimeStore>((set) => ({
-  currentTime: "",
-  fetchTime: async () => {
-    try {
-      const response = await fetch(
-        "http://worldtimeapi.org/api/timezone/Europe/London",
-      );
-      const data = await response.json();
-      set({ currentTime: data.datetime });
-    } catch (error) {
-      console.error("Error fetching time:", error);
-    }
+  currentTime: new Date().toLocaleString(),
+  fetchTime: () => {
+    const localTime = new Date().toLocaleString(); 
+    set({ currentTime: localTime });
   },
 }));
